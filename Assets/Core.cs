6d7d1ffd41952c1ddd14843c923ba94e2d91d2ae
kekland <kk.erzhan@gmail.com>
2018-01-07 15:44:11 +0600

    First commit

---
+++ Assets/Core.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NeuralNetwork;
+
+public class Core : MonoBehaviour {
+
+	public RobotController RobotObject;
+
+	public Vector3 RobotStartPosition;
+	public float SecondsElapsed;
+	float TimeStart, TimeEnd;
+
+	public bool IS_NEURAL_PLAYING = false;
+
+	NeuralNet net;
+	List<DataSet> trainData = new List<DataSet>();
+	void Awake() {
+		if(!IS_NEURAL_PLAYING) {
+			return;
+		}
+		net = new NeuralNet(2, 8, 4);
+		//Inputs: DOES_SEE_CORNER DOES_SEE_ROBOT
+		//Outputs: FWD BWD LEF RIG
+		trainData.Add(new DataSet(new double[] { 0, 0 }, new double[] { 0, 0, 1, 0 }));
+		trainData.Add(new DataSet(new double[] { 1, 0 }, new double[] { 0, 1, 0, 0 }));
+		trainData.Add(new DataSet(new double[] { 0, 1 }, new double[] { 1, 0, 0, 0 }));
+		trainData.Add(new DataSet(new double[] { 1, 1 }, new double[] { 0, 1, 0, 0 }));
+
+		net.Train(trainData, 0.01);
+	}
+
+	void Start () {
+		ResetGame();
+	}
+
+	void Update () {
+		ProcessUserInput();
+		CheckRobotDeath();
+	}
+
+	void ProcessUserInput() {
+		if(IS_NEURAL_PLAYING) {
+			int seesCorner = RobotObject.CornerScannerSensorData ? 1 : 0;
+			int seesRobot = RobotObject.RobotScannerSensorData ? 1 : 0;
+			double[] values = net.Compute(new double[] { seesCorner, seesRobot });
+
+			float verticalAxis = (float)values[0] - (float)values[1];
+			float horizontalAxis = (float)values[3] - (float)values[2];
+
+			RobotObject.Move(verticalAxis);
+			RobotObject.Turn(horizontalAxis);
+			return;
+		}
+		RobotObject.Move(Input.GetAxis("Vertical"));
+		RobotObject.Turn(Input.GetAxis("Horizontal"));
+	}
+
+	void CheckRobotDeath() {
+		GameObject UnderObject = RobotObject.GetObjectUnderRobot();
+		if(UnderObject == null) {
+			TimeEnd = Time.time;
+			Debug.Log(TimeEnd - TimeStart);
+			ResetGame();
+		}
+	}
+
+	void ResetGame() {
+		RobotObject.transform.position = RobotStartPosition;
+		TimeStart = Time.time;
+	}
+}
---
+++ Assets/RobotController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+
+	public GameObject CornerScanner;
+	MeshRenderer CornerScannerRenderer;
+
+	public GameObject RobotScanner;
+	MeshRenderer RobotScannerRenderer;
+
+	public float Velocity;
+	public float AngularVelocity;
+
+	public bool CornerScannerSensorData;
+	public bool RobotScannerSensorData;
+
+	public LayerMask CornerScannerLayerMask;
+	public LayerMask RobotScannerLayerMask;
+	Rigidbody robotRB;
+	void Start()
+	{
+		robotRB = GetComponent<Rigidbody>();
+		CornerScannerRenderer = CornerScanner.GetComponent<MeshRenderer>();
+		RobotScannerRenderer = RobotScanner.GetComponent<MeshRenderer>();
+	}
+
+	void Update()
+	{
+		CornerScannerSensorData = DidCornerScannerHitCorner();
+		RobotScannerSensorData = DidRobotScannerHitRobot();
+		ModifyScannerMaterial();
+	}
+
+	bool DidCornerScannerHitCorner()
+	{
+
+		//Construct ray object
+		Ray ray = new Ray(CornerScanner.transform.position, new Vector3(0f, -1f, 0f));
+		return !Physics.Raycast(ray, CornerScannerLayerMask.value);
+	}
+
+	bool DidRobotScannerHitRobot()
+	{
+		RaycastHit hit;
+
+		//Construct ray object
+		Ray ray = new Ray(RobotScanner.transform.position, transform.forward);
+		return Physics.Raycast(ray, RobotScannerLayerMask.value);
+	}
+
+
+	public Material ScannerHitCornerMaterial;
+	public Material ScannerDefaultMaterial;
+	void ModifyScannerMaterial()
+	{
+		//If scanner did hit corner - substitute material with red, otherwise - green
+		CornerScannerRenderer.material = CornerScannerSensorData ?  ScannerDefaultMaterial : ScannerHitCornerMaterial;
+		RobotScannerRenderer.material = RobotScannerSensorData ? ScannerDefaultMaterial : ScannerHitCornerMaterial;
+	}
+
+	public void Move(float VelocityMultiplier)
+	{
+		VelocityMultiplier = Mathf.Clamp(VelocityMultiplier, -1f, 1f);
+		//robotRB.AddRelativeForce(new Vector3(0f, 0f, VelocityMultiplier * Velocity));
+		robotRB.MovePosition(transform.position + (VelocityMultiplier * Velocity) * transform.forward * Time.deltaTime);
+		//transform.position += (VelocityMultiplier * Velocity) * transform.forward * Time.deltaTime;
+	}
+
+	public void Turn(float AngularMultiplier)
+	{
+		AngularMultiplier = Mathf.Clamp(AngularMultiplier, -1f, 1f);
+		//robotRB.AddRelativeTorque(0f, AngularVelocity * AngularMultiplier, 0f);
+		robotRB.MoveRotation(Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, AngularVelocity * AngularMultiplier * Time.deltaTime, 0f)));
+		//transform.Rotate(0f, AngularVelocity * AngularMultiplier * Time.deltaTime, 0f);
+	}
+
+	public GameObject GetObjectUnderRobot()
+	{
+		RaycastHit hit;
+
+		//Construct ray object
+		Ray ray = new Ray(transform.position, new Vector3(0f, -1f, 0f));
+		if (Physics.Raycast(ray, out hit))
+		{
+			//If scanner hit corner - return true and vice versa
+			return hit.collider.gameObject;
+		}
+		return null;
+	}
+}
